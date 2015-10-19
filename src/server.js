ed097abbf613d74393249c5023d79aafefead106
Kristóf Poduszló <kripod@protonmail.com>
2015-10-19 19:15:37 +0200

    Renamed some files to make things work on UNIX systems

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import PlayerIO from './playerio/PlayerIO';
 
-PlayerIO.QuickConnect.simpleConnect('everybody-edits-su9rn58o40itdbnw69plyw', 'guest1@tbp.com', 'guest', null, function(client) {
-	client.multiplayer.joinRoom('PWAJwkxnxta0I', null, function(connection) {
+PlayerIO.QuickConnect.simpleConnect('everybody-edits-su9rn58o40itdbnw69plyw', 'guest1@tbp.com', 'guest', null, function (client) {
+	client.multiplayer.joinRoom('PWAJwkxnxta0I', null, function (connection) {
 		connection.send(new PlayerIO.Message('init', []));
-		
+
 		connection.on('message', function (m) {
-			if (m.type == "init") {				
-				connection.send(new PlayerIO.Message('say', ["HELLO FROM JAVASCRIPT!"]));
+			if (m.type == 'init') {
+				connection.send(new PlayerIO.Message('say', ['HELLO FROM JAVASCRIPT!']));
 			}
 		});
 	}, function (playerIOError) {
