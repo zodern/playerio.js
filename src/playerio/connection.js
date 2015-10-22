cfe5d7270e4566625444c02b93c11b0eda9c07e5
Kristóf Poduszló <kripod@protonmail.com>
2015-10-22 19:39:07 +0200

    Added a way to chat (one-way); rewrote Connection

--- src/playerio/connection.js
+++ src/playerio/connection.js
@@ -1,64 +1,68 @@
-var util = require('util'),
-	events = require('events'),
-	net = require('net'),
-	Message = require('./models/message'),
-	PlayerIOError = require('./models/playerio-error'),
-	BinarySerializer = require('./helpers/binary-serializer');
+/* @flow */
 
-function Connection(endpoint, joinKey, joinData) {
-	events.EventEmitter.call(this);
-				
-	this.connected = false;
-	var that = this;
-	
-	var serializer = new BinarySerializer();
-	serializer.on('message', function (message) {
-		if (!that.connected && message.type == "playerio.joinresult") {
-			if (!message.items[0]) {
-				that.error = new PlayerIOError(message.items[1], message.items[2]);
-				that.disconnect();
+import net from 'net';
+import { EventEmitter } from 'events';
+import Message from './models/message';
+import PlayerIOError from './models/playerio-error';
+import BinarySerializer from './helpers/binary-serializer';
+
+export default class Connection extends EventEmitter {
+	constructor(endpoint, joinKey, joinData) {
+		super();
+
+		this.connected = false;
+
+		let self = this;
+		let serializer = new BinarySerializer();
+
+		serializer.on('message', function (message) {
+			if (!self.connected && message.type === 'playerio.joinresult') {
+				if (!message.items[0]) {
+					self.error = new PlayerIOError(message.items[1], message.items[2]);
+					self.disconnect();
+				} else {
+					self.connected = true;
+					self.emit('connect');
+				}
+
 			} else {
-				that.connected = true;
-				that.emit('connect');				
+				self.emit('message', message);
 			}
-		} else {
-			that.emit('message', message);
-		}
-	})
-		
-	var sock = net.connect(endpoint.port, endpoint.address);
-	sock.on('connect', function () {
-		sock.write(new Buffer([0]));
-		var msg = new Message('join', joinKey);
-		for (var kv in joinData) {
-			msg.add(kv.key);
-			msg.add(kv.value);
-		}
-		that.send(msg);
-	});
-	
-	sock.on('data', function (data) {		
-		serializer.addBytes(data);
-	});
-	
-	sock.on('close', function (close) {
-		if (that.connected) {		
-			that.connected = false;
-			that.emit('disconnect');
-		} else {
-			that.error = that.error | new PlayerIOError(0, "Error connecting to server.");
-			that.emit('error');
-		}
-	});	
-	
-	this.send = function (message) {
-		sock.write(BinarySerializer.serializeMessage(message));
-	}
-	
-	this.disconnect = function () {
-		this.sock.close();
-	}	
-}
-util.inherits(Connection, events.EventEmitter);
+		});
 
-module.exports = Connection;
+		let sock = net.connect(endpoint.port, endpoint.address);
+		this.sock = sock;
+
+		sock.on('connect', function () {
+			sock.write(new Buffer([0]));
+			let msg = new Message('join', joinKey);
+			for (let kv in joinData) {
+				msg.add(kv.key);
+				msg.add(kv.value);
+			}
+			self.send(msg);
+		});
+
+		sock.on('data', function (data) {
+			serializer.addBytes(data);
+		});
+
+		sock.on('close', function (close) {
+			if (self.connected) {
+				self.connected = false;
+				self.emit('disconnect');
+			} else {
+				self.error = self.error | new PlayerIOError(0, 'Error connecting to server.');
+				self.emit('error');
+			}
+		});
+	}
+
+	send(message) {
+		this.sock.write(BinarySerializer.serializeMessage(message));
+	}
+
+	disconnect() {
+		this.sock.close();
+	}
+}
--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
+import readline from 'readline';
 import PlayerIO from './playerio/playerio';
+
+let rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+});
 
 PlayerIO.QuickConnect.simpleConnect('everybody-edits-su9rn58o40itdbnw69plyw', 'guest1@tbp.com', 'guest', null, function (client) {
 	client.multiplayer.joinRoom('PWAJwkxnxta0I', null, function (connection) {
 		connection.on('message', function (m) {
 			if (m.type === 'init') {
-				connection.send(new PlayerIO.Message('say', 'HELLO FROM JAVASCRIPT!'));
+				// Allow custom chat messages to be sent
+				console.log('[INFO] Now you can start chatting.');
+				rl.on('line', function (line) {
+					connection.send(new PlayerIO.Message('say', line));
+				});
 			}
 		});
 
