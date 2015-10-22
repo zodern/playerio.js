05b63bf3e07e2b44636aa2a174e7ba8176a0d973
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2015-10-22 16:34:48 +0200

    Add jsconfig, fix vscode

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import PlayerIO from './playerio/playerio';
 
 PlayerIO.QuickConnect.simpleConnect('everybody-edits-su9rn58o40itdbnw69plyw', 'guest1@tbp.com', 'guest', null, function (client) {
-	client.multiplayer.joinRoom('PWJ3pjzc59bUI', null, function (connection) {
+	client.multiplayer.joinRoom('PWAJwkxnxta0I', null, function (connection) {
 		connection.send(new PlayerIO.Message('init'));
 
 		connection.on('message', function (m) {
